e55829833a5649d3bc96cc1b9f0b9ddc9f062d1d
petergdoyle <peter.doyle@travelport.com>
2017-08-19 09:18:24 -0600

    script needed +w

--- zip_binary_dist.sh
+++ zip_binary_dist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd ..
-zip -r mssql-java-client-binaries.zip --exclude=*sqljdbc_6.0.zip --exclude=*.git*  --exclude=*.vagrant* mssql-java-client/
-cd - 
+zip -r mssql-java-client/mssql-java-client-binaries.zip --exclude=*sqljdbc_6.0.zip --exclude=*.git*  --exclude=*.vagrant* mssql-java-client/
+cd -
