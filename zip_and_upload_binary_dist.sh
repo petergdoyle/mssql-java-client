74147689ad284dbc848bdf7bffdd8f1e1a61980c
petergdoyle <peter.doyle@travelport.com>
2017-08-22 08:37:36 -0600

    modified to do all in one binary dist work

--- zip_and_upload_binary_dist.sh
+++ zip_and_upload_binary_dist.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+if [ -f mssql-java-client-binaries.zip ]; then
+  rm -fv mssql-java-client-binaries.zip
+fi
 cd ..
 zip -r mssql-java-client/mssql-java-client-binaries.zip --exclude=*sqljdbc_6.0.zip --exclude=*.git*  --exclude=*.vagrant* mssql-java-client/
 cd -
+scp mssql-java-client-binaries.zip petergdoyle@streamworksnode0.westus.cloudapp.azure.com:
